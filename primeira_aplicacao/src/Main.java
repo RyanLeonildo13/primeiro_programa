d9a8cb4291ef969e6340a05aeea4e88f6d4d4059
King <King@King>
2024-02-15 13:41:44 -0300

    primeiro commit

---
+++ primeira_aplicacao/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		
+//"println" é usado para que quebre a linha, caso contrário, use "print"
+
+		System.out.println("Olá Nessa Bagaça!!!");
+		
+		int y = 32;
+		double x = 10.54545;
+		
+		System.out.println(y);
+		System.out.println(x);
+		
+//usado para definir a quantidade de casas decimais "%.2f%n"
+		
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.3f%n", x);
+	}
+
+}
